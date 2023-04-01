a44868967e84ec89e5931bace79e2d7a02d05770
Farah Asylah <farahasylah@gmail.com>
2023-04-01 19:38:45 +0800

    remove unwanted plugin

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,28 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/Vue-Timezone-Weather-Compare/",
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+    svgRule.delete('type');
+    svgRule.delete('generator');
+
+    svgRule
+     .oneOf('inline')
+     .resourceQuery(/inline/)
+     .use('babel-loader')
+     .loader('babel-loader')
+     .end()
+     .use('vue-svg-loader')
+     .loader('vue-svg-loader')
+     .end()
+     .end()
+     .oneOf('external')
+     .use('file-loader')
+     .loader('file-loader')
+     .options({
+       name: 'assets/[name].[hash:8].[ext]',
+     });
+   },
 })
